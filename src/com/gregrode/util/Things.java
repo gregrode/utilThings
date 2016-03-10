ff1d62ca3065a94a3dbbac4275c2d4f83d1b2062
Dennis, Greg <gdennis@bottomline.com>
2016-03-10 18:13:38 -0500

    Updated documentation and added new functionalities.

--- src/com/gregrode/util/Things.java
+++ src/com/gregrode/util/Things.java
@@ -8,6 +8,7 @@ import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -18,13 +19,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * The <code>Things</code> class provides functionality to create and verify
- * some of your favorite Java objects
- * 
+ * The <code>Things</code> class provides functionality to create and verify some of your favorite Java objects
+ *
  * @author Gregroy Dennis<br>
  *         Last updated: March 04, 2016<br>
  *         &copy; Gregroy Dennis 2016
- * 
  */
 public final class Things
 {
@@ -36,153 +35,160 @@ public final class Things
 	}
 
 	/**
-	 * Check whether the given object is null or not. if null, an
-	 * <code>ErrorStatusException</code> is thrown.
-	 * 
+	 * Check whether the given object is null or not. if null, an {@link NullPointerException} is thrown.
+	 *
 	 * @param obj
 	 *            the object to check
-	 * @return the object
+	 * @param <T>
+	 *            the type of object
+	 * @return T
 	 */
-	public static <T> T verify(T obj)
+	public static <T> T verify(T t)
 	{
-		return verify(obj, new NullPointerException());
+		return verify(t, new NullPointerException());
 	}
 
 	/**
-	 * Check whether the given object is null or not. if null, an
+	 * Check whether the given object is null or not. if null, an {@link NullPointerException} is thrown.
+	 *
+	 * @param obj
+	 *            the object to check
+	 * @param message
+	 *            the message within the exception
+	 * @param <T>
+	 *            the type of object
+	 * @return T
+	 */
+	public static <T> T verify(T t, String message)
+	{
+		return verify(t, new NullPointerException(message));
+	}
+
+	/**
+	 * Check whether the given object is null or valid based on the given predicate. if the predicate fails, an
 	 * {@link NullPointerException} is thrown.
-	 * 
-	 * @param obj
-	 *            the object to check
-	 * @param message
-	 *            the message within the exception
-	 * @return obj
-	 */
-	public static <T> T verify(T obj, String message)
-	{
-		return verify(obj, new NullPointerException(message));
-	}
-
-	/**
-	 * Check whether the given object is null or valid based on the given
-	 * predicate. if the predicate fails, an {@link NullPointerException} is
-	 * thrown.
-	 * 
-	 * @param obj
+	 *
+	 * @param t
 	 *            the object to check
 	 * @param predicate
 	 *            the predicate to test
-	 * @return obj
+	 * @param <T>
+	 *            the type of object
+	 * @return T
 	 */
-	public static <T> T verify(T obj, Predicate<T> predicate)
+	public static <T> T verify(T t, Predicate<T> predicate)
 	{
-		return verify(obj, new NullPointerException(), predicate);
+		return verify(t, new NullPointerException(), predicate);
 	}
 
 	/**
-	 * Check whether the given object is null or valid based on the given
-	 * predicate. if the predicate fails, a {@link NullPointerException} is
-	 * thrown.
-	 * 
-	 * @param obj
+	 * Check whether the given object is null or valid based on the given predicate. if the predicate fails, a
+	 * {@link NullPointerException} is thrown.
+	 *
+	 * @param t
 	 *            the object to check
 	 * @param message
 	 *            the message within the exception
 	 * @param predicate
 	 *            the predicate to test
-	 * @return obj
+	 * @param <T>
+	 *            the type of object
+	 * @return T
 	 */
-	public static <T> T verify(T obj, String message, Predicate<T> predicate)
+	public static <T> T verify(T t, String message, Predicate<T> predicate)
 	{
-		return verify(obj, new NullPointerException(message), predicate);
+		return verify(t, new NullPointerException(message), predicate);
 	}
 
 	/**
-	 * Check whether the given object is null or not. if null, an
-	 * <code>RuntimeException</code> is thrown.
-	 * 
-	 * 
-	 * @param obj
+	 * Check whether the given object is null or not. if null, an {@link RuntimeException} is thrown.
+	 *
+	 * @param t
 	 *            the object to check
 	 * @param exception
-	 *            the RuntimeException to throw
-	 * @return object
+	 *            the {@link RuntimeException} to throw
+	 * @param <T>
+	 *            the type of object
+	 * @return T
 	 */
-	public static <T> T verify(T obj, RuntimeException exception)
+	public static <T> T verify(T t, RuntimeException exception)
 	{
-		return verify(obj, exception, o -> true);
+		return verify(t, exception, o -> true);
 	}
 
 	/**
-	 * 
-	 * This method will throw an exception if one of the following cases occurs:
+	 *
+	 * This method will throw an instance of {@link RuntimeException} if one of the following cases occurs:
 	 * <ol>
 	 * <li>The object is null</li>
-	 * <li>The object is an instance of a {@link Boolean} and the value is false
-	 * </li>
-	 * <li>The object is an instance of a {@link String} and the value is empty
-	 * </li>
-	 * <li>The object is an instance of a {@link Collection} and the value is
-	 * empty</li>
+	 * <li>The object is an instance of a {@link Boolean} and the value is false</li>
+	 * <li>The object is an instance of a {@link String} and the value is empty</li>
+	 * <li>The object is an instance of a {@link Collection} and the value is empty</li>
 	 * <li>The object is an instance of a {@link Map} the value is empty</li>
 	 * <li>The result from the predicate is false</li>
 	 * <li>The given exception is null</li>
 	 * </ol>
-	 * 
-	 * 
-	 * @param obj
+	 *
+	 *
+	 * @param t
 	 *            the object to check
 	 * @param exception
-	 *            the RuntimeException to throw * @param predicate the predicate
-	 *            to test
-	 * @return object
+	 *            the {@link RuntimeException} to throw
+	 * @param predicate
+	 *            the {@link Predicate} object to test
+	 * @param <T>
+	 *            the type of object
+	 * @return T
 	 */
-	public static <T> T verify(T obj, RuntimeException exception, Predicate<T> predicate)
+	public static <T> T verify(T t, RuntimeException exception, Predicate<T> predicate)
 	{
-		boolean bool = (obj != null);
-		if (bool && (obj instanceof Boolean))
+		boolean bool = (t != null);
+		if (bool && (t instanceof Boolean))
 		{
-			bool = ((Boolean) obj).booleanValue();
+			bool = ((Boolean) t).booleanValue();
 		}
 
-		if (bool && (obj instanceof String))
+		if (bool && (t instanceof String))
 		{
-			bool = !((String) obj).isEmpty();
+			bool = !((String) t).isEmpty();
 		}
 
-		if (bool && (obj instanceof Collection))
+		if (bool && (t instanceof Collection))
 		{
-			bool = !((Collection<?>) obj).isEmpty();
+			bool = !((Collection<?>) t).isEmpty();
 		}
 
-		if (bool && (obj instanceof Map))
+		if (bool && (t instanceof Map))
 		{
-			bool = !((Map<?, ?>) obj).isEmpty();
+			bool = !((Map<?, ?>) t).isEmpty();
 		}
 
-		if (!bool || ((predicate != null) && !predicate.test(obj)))
+		if (!bool || ((predicate != null) && !predicate.test(t)))
 		{
 			throw verify(exception, "Exception was not specified.");
 		}
-		return obj;
+		return t;
 	}
 
 	/**
 	 * Get the first non null value with in the given varargs
-	 * 
-	 * @param things
-	 *            the list of thing
-	 * @return first non null thing
+	 *
+	 * @param items
+	 *            the list of items
+	 * @param <T>
+	 *            the type of object
+	 * @return T
 	 */
 	@SafeVarargs
-	public static <T> T either(T... things)
+	public static <T> T either(T... items)
 	{
-		return Stream.of(things).filter(t -> t != null).findFirst().get();
+		return Stream.of(verify(items, new IllegalArgumentException("Items not specified."))).filter(t -> t != null).findFirst()
+			.get();
 	}
 
 	/**
 	 * Get the first non zero integer within the given varargs
-	 * 
+	 *
 	 * @param things
 	 *            the list of i
 	 * @return first non zero integer
@@ -194,29 +200,123 @@ public final class Things
 	}
 
 	/**
-	 * Create an implementation of the {@link Map} interface using the given
-	 * {@Supplier} object and populate the map with the given list of
-	 * {@link Entry}
-	 * 
+	 * Create an implementation of the {@link Map} interface using the given {@link Supplier} object and populate the map with the
+	 * given list of {@link Entry}
+	 *
 	 * @param mapSupplier
 	 *            The implementation of the {@link Map} interface to user
 	 * @param entries
 	 *            the Key/value pair to populate the map with
+	 * @param <K>
+	 *            The type of object for the map key
+	 * @param <V>
+	 *            The type of object for the map value
+	 * @param <M>
+	 *            The type of object for the map
 	 * @return Map
 	 */
 	@SafeVarargs
-	public static <K, V, M extends Map<K, V>> Map<K, V> toMap(Supplier<M> mapSupplier, Entry<K, V>... entries)
+	public static <K, V, M extends Map<K, V>> M toMap(Supplier<M> mapSupplier, Entry<K, V>... entries)
 	{
-		return Stream.of(verify(entries, "Entries not specified")).collect(
-		        Collectors.toMap(Entry::getKey, Entry::getValue, (m, m2) -> m, verify(mapSupplier, "Implementation of Map was not specified. ")));
+		return toMap(mapSupplier, Entry::getKey, Entry::getValue, entries);
 	}
 
 	/**
-	 * Create a HashMap from the given JSON object
+	 * Create an implementation of the {@link Map} interface using the given {@link Supplier} object and populate the map with the
+	 * given Array of T objects
+	 *
+	 * @param mapSupplier
+	 *            The implementation of the {@link Map} interface that will be used
+	 * @param keyMapper
+	 *            The keyMapper indicates how the key will be resolved
+	 * @param valueMapper
+	 *            the valueMapper indicates how the value will be resolved.
+	 * @param items
+	 *            an Array of T
+	 *
+	 * @param <K>
+	 *            The type of object for the map key
+	 * @param <V>
+	 *            The type of object for the map value
+	 * @param <M>
+	 *            The type of object for the map
+	 * @param <T>
+	 *            the type of object in the Collection
+	 * @return {@link Map}
+	 */
+	@SafeVarargs
+	public static <K, V, M extends Map<K, V>, T> M toMap(Supplier<M> mapSupplier, Function<T, K> keyMapper,
+			Function<T, V> valueMapper, T... items)
+	{
+		return toMap(mapSupplier, keyMapper, valueMapper, Arrays.asList(verify(items)));
+	}
+
+	/**
+	 * Create a {@link HashMap} implementation of the {@link Map} interface using the given the {@link Function} objects to
+	 * determine the key/value and populate the map with the given {@link Collection}.
+	 *
+	 * @param keyMapper
+	 *            The keyMapper indicates how the key will be resolved
+	 * @param valueMapper
+	 *            the valueMapper indicates how the value will be resolved.
+	 * @param items
+	 *            the collection of items
+	 *
+	 * @param <K>
+	 *            The type of object for the map key
+	 * @param <V>
+	 *            The type of object for the map value
+	 * @param <T>
+	 *            the type of object in the Collection
+	 * @param <C>
+	 *            The type of object for the Collection
+	 * @return {@link Map}
+	 */
+	public static <K, V, T, C extends Collection<T>> Map<K, V> toMap(Function<T, K> keyMapper, Function<T, V> valueMapper,
+			C items)
+	{
+		return toMap(HashMap::new, keyMapper, valueMapper, items);
+	}
+
+	/**
+	 * Create an implementation of the {@link Map} interface using the given {@link Supplier} object, the {@link Function} objects
+	 * to determine the key/value and populate the map with the given {@link Collection}.
+	 *
+	 * @param mapSupplier
+	 *            The implementation of the {@link Map} interface that will be used
+	 * @param keyMapper
+	 *            The keyMapper indicates how the key will be resolved
+	 * @param valueMapper
+	 *            the valueMapper indicates how the value will be resolved.
+	 * @param items
+	 *            the collection of items
+	 * @param <K>
+	 *            The type of object for the map key
+	 * @param <V>
+	 *            The type of object for the map value
+	 * @param <M>
+	 *            The type of object for the map
+	 * @param <T>
+	 *            the type of object in the Collection
+	 * @param <C>
+	 *            The type of object for the Collection
+	 * @return {@link Map}
+	 */
+	public static <K, V, M extends Map<K, V>, T, C extends Collection<T>> M toMap(Supplier<M> mapSupplier,
+			Function<T, K> keyMapper, Function<T, V> valueMapper, C items)
+	{
+		return verify(items, new IllegalArgumentException("Collection not specified.")).stream()
+			.collect(Collectors.toMap(verify(keyMapper, new IllegalArgumentException("Key mapper not specified.")),
+				verify(valueMapper, new IllegalArgumentException("Value mapper not specified.")), (m, m2) -> m,
+				verify(mapSupplier, new IllegalArgumentException("Implementation of Map was not specified."))));
+	}
+
+	/**
+	 * Create a {@link HashMap} from the given JSON object
 	 *
 	 * @param json
 	 *            the JSON String
-	 * @return Map
+	 * @return {@link Map}
 	 */
 	public static Map<String, String> toMap(String json)
 	{
@@ -224,12 +324,16 @@ public final class Things
 	}
 
 	/**
-	 * Create an implementation of the {@link Map} interface using the given
-	 * {@Supplier} object and populate the map with the given JSON
+	 * Create an implementation of the {@link Map} interface using the given {@link Supplier} object and populate the map with the
+	 * given JSON
 	 *
 	 * @param json
 	 *            the JSON String
-	 * @return Map
+	 * @param <K>
+	 *            The type of object for the map key.
+	 * @param <V>
+	 *            The type of object for the map value
+	 * @return {@link Map}
 	 */
 	@SuppressWarnings("unchecked")
 	public static <K, V, M extends Map<K, V>> Map<K, V> toMap(Supplier<M> mapSupplier, String json)
@@ -238,7 +342,7 @@ public final class Things
 		{
 			final ObjectMapper mapper = new ObjectMapper();
 			final JsonNode jsonNode = mapper.readTree(verify(json, "Cannot transform null string in Map.").replaceAll("'", "\""));
-			return mapper.treeToValue(jsonNode, mapSupplier.get().getClass());
+			return mapper.treeToValue(jsonNode, verify(mapSupplier).get().getClass());
 		}
 		catch (final IOException e)
 		{
@@ -247,17 +351,24 @@ public final class Things
 	}
 
 	/**
-	 * Create a {@link EnumMap} implementation of the {@link Map} interface
-	 * using the enum constants as the keys and the result of the
-	 * {@link Function#apply(Object)} as the value.
-	 * 
+	 * Create a {@link EnumMap} implementation of the {@link Map} interface using the enum constants as the keys and the result of
+	 * the {@link Function#apply(Object)} as the value.
+	 *
 	 * @param clazz
+	 *            the class of the enum
 	 * @param function
-	 * @return
+	 *            the functional interface that indicates how the value will be resolved.
+	 *
+	 * @param <K>
+	 *            The type of object for the map key. Note, in this case the key will be constant within the enum.
+	 * @param <V>
+	 *            The type of object for the map value
+	 *
+	 * @return {@link Map}
 	 */
-	public static <K extends Enum<K>, R> Map<K, R> toMap(Class<K> clazz, Function<K, R> function)
+	public static <K extends Enum<K>, V> Map<K, V> toMap(Class<K> clazz, Function<K, V> function)
 	{
-		final EnumMap<K, R> map = new EnumMap<K, R>(verify(clazz, "Enum class not specified."));
+		final EnumMap<K, V> map = new EnumMap<K, V>(verify(clazz, "Enum class not specified."));
 		Arrays.stream(clazz.getEnumConstants()).forEach(v -> {
 			map.put(v, verify(function, "Function lambda not specified.").apply(v));
 		});
@@ -265,14 +376,18 @@ public final class Things
 	}
 
 	/**
-	 * Create a {@link Entry} object using the {@link AbstractMap.SimpleEntry}
-	 * implementation.
-	 * 
+	 * Create a {@link Entry} object using the {@link AbstractMap.SimpleEntry} implementation.
+	 *
 	 * @param key
 	 *            The key
 	 * @param value
 	 *            the value
-	 * @return Entry
+	 *
+	 * @param <K>
+	 *            The type of object for the key.
+	 * @param <V>
+	 *            The type of object for the value.
+	 * @return {@link Entry}
 	 */
 	public static <K, V> Entry<K, V> pair(K key, V value)
 	{
@@ -297,6 +412,32 @@ public final class Things
 		{
 			return EMPTY;
 		}
+	}
+
+	/**
+	 * Pluck value out of the given {@link Collection} based on the given {@link Function} object and return them as a Collection
+	 *
+	 * @param items
+	 *            the collection of itmes
+	 * @param function
+	 *            the functional interface that determine what value will be plucked.
+	 * @return {@link Collection}
+	 */
+	public static <T, R> Collection<R> pluck(Collection<T> items, Function<T, R> function)
+	{
+		return verify(items, new IllegalArgumentException("Collection not specified")).stream().collect(Collectors
+			.mapping(verify(function, new IllegalArgumentException("function lambda not specified")), Collectors.toList()));
+	}
+
+	/**
+	 * @param t
+	 * @param consumer
+	 * @return
+	 */
+	public static <T> T build(T t, Consumer<T> consumer)
+	{
+		consumer.accept(t);
+		return t;
 	}
 
 }
--- test/com/gregrode/util/ThingsTest.java
+++ test/com/gregrode/util/ThingsTest.java
@@ -1,10 +1,14 @@
 /**
- * 
+ *
  */
 package com.gregrode.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
@@ -12,17 +16,47 @@ import org.junit.Test;
 
 public final class ThingsTest
 {
+	enum Color
+	{
+		BLUE, RED, GREEN
+	}
 
 	@Test(expected = NullPointerException.class)
 	public void verifyTest()
 	{
+		Assert.assertNotNull(Things.verify("test"));
+		Assert.assertNotNull(Things.verify("test", s -> !s.isEmpty()));
+		Assert.assertNotNull(Things.verify("test", "null", s -> !s.isEmpty()));
+		Assert.assertTrue(Things.verify(true));
+		Assert.assertNotNull(Things.verify(Things.toMap(LinkedHashMap::new, Things.pair("firstName", "greg"))));
 		Things.verify("test", (str) -> str.length() == 5);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void verifyIllegalArgumentExceptionTest()
+	{
+		Things.verify(null, new IllegalArgumentException());
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void verifyListTest()
+	{
+		Things.verify(new ArrayList<String>(0), "null");
+	}
+
+	@Test
+	public void pluckTest()
+	{
+		final List<Map.Entry<String, String>> entries = Arrays.asList(Things.pair("firstname", "greg"),
+			Things.pair("lastName", "Dennis"));
+		final Collection<String> keys = Things.pluck(entries, Map.Entry::getKey);
+		Assert.assertNotNull(keys);
 	}
 
 	@Test
 	public void toJSONTest()
 	{
-		String json = Things.toJSON("{'home' : 'house', 'vehicle' : 'car'}");
+		final String json = Things.toJSON("{'home' : 'house', 'vehicle' : 'car'}");
 		Assert.assertNotNull(json);
 	}
 
@@ -30,7 +64,8 @@ public final class ThingsTest
 	public void toMapTest()
 	{
 
-		final Map<String, String> map = Things.toMap(LinkedHashMap::new, Things.pair("firstName", "greg"), Things.pair("lastName", "Dennis"));
+		final Map<String, String> map = Things.toMap(LinkedHashMap::new, Things.pair("firstName", "greg"),
+			Things.pair("lastName", "Dennis"));
 		Assert.assertTrue(map.containsKey("firstName"));
 		Assert.assertEquals("greg", map.get("firstName"));
 
@@ -38,15 +73,20 @@ public final class ThingsTest
 		Assert.assertTrue(map2.containsKey("home"));
 		Assert.assertEquals("house", map2.get("home"));
 
-		final Map<String, String> map3 = Things.toMap(HashMap::new, "{'1': 'apple', '4' : 'zebra', '7' : 'queens', '100' : 'baseball'}");
+		final Map<Integer, String> map3 = Things.toMap(HashMap::new,
+			"{'1': 'apple', '4' : 'zebra', '7' : 'queens', '100' : 'baseball'}");
 		Assert.assertEquals("apple", map3.get("1"));
 
-		Map<Color, Integer> map4 = Things.toMap(Color.class, Color::hashCode);
+		final Map<Color, Integer> map4 = Things.toMap(Color.class, Color::hashCode);
 		Assert.assertTrue(map4.containsKey(Color.RED));
 	}
 
-	enum Color
+	@Test
+	public void buildTest()
 	{
-		BLUE, RED, GREEN
+		final List<String> list = new ArrayList<>();
+		list.add("Greg");
+		list.add("Dennis");
+
 	}
 }
